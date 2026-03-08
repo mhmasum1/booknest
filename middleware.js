35f46640e4a6f5a63917bb1310724f8cac011e30
mhmasum1 <mhmasum231@gmail.com>
2026-03-09 02:27:47 +0600

    fix middleware auth

--- middleware.js
+++ middleware.js
@@ -3,6 +3,9 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(
     function middleware(req) { },
     {
+        callbacks: {
+            authorized: ({ token }) => !!token,
+        },
         pages: {
             signIn: "/login",
         },
